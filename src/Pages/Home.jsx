e730461794643c3a5e21e73c74203558c1414140
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-26 08:55:46 +0600

    create booksCard component for show all books

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../components/Banner/Banner";
+import BooksCards from "../components/BooksCards/BooksCards";
 
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div className="mt-10">
             {/* Banner */}
             <Banner></Banner>
+            <BooksCards></BooksCards>
         </div>
     );
 };
---
+++ src/components/BooksCards/BooksCards.jsx
@@ -0,0 +1,11 @@
+
+
+const BooksCards = () => {
+    return (
+        <div className="mt-14">
+            <h3 className="text-center font-bold text-4xl">Books</h3>
+        </div>
+    );
+};
+
+export default BooksCards;
