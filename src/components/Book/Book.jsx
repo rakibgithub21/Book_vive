3bd447ca5d903c7f88093ca941f9001bfdd5583f
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-26 09:03:38 +0600

    maping books and create book component for show each book

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,11 @@
+
+
+const Book = ({book}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Book;
--- src/components/BooksCards/BooksCards.jsx
+++ src/components/BooksCards/BooksCards.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 
 const BooksCards = () => {
-    const [books, setBooks] = useState();
+    const [books, setBooks] = useState([]);
     useEffect(() => {
         fetch('allbooks.json')
             .then(res => res.json())
@@ -12,6 +13,14 @@ const BooksCards = () => {
     return (
         <div className="mt-14">
             <h3 className="text-center font-bold text-4xl">Books</h3>
+            <div>
+                {
+                    books.map(book => <Book
+                        key={book.bookId}
+                        book={book}
+                    ></Book>)
+                }
+            </div>
         </div>
     );
 };
