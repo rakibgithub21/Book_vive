309250c0b29a46db777d6d32a25cb7db63153058
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-26 17:53:53 +0600

    read book page mobile responsive

--- src/Pages/Read.jsx
+++ src/Pages/Read.jsx
@@ -8,18 +8,18 @@ const Read = ({ read }) => {
     console.log(read);
     const { image, yearOfPublishing, totalPages, tags, rating, bookName, author, publisher, category, bookId } = read;
     return (
-        <div className="border my-5 gap-5 flex border-[#13131326] p-5 rounded-2xl">
+        <div className="border my-5 gap-5 flex flex-col md:flex-row border-[#13131326] p-5 rounded-2xl">
             <div className="bg-[#1313130d] p-10 rounded-2xl">
-                <img src={image} alt="" />
+                <img className="w-full" src={image} alt="" />
             </div>
             <div className="space-y-5">
                 <h1 className="font-bold text-2xl">{bookName}</h1>
                 <p className="text-[#131313cc] font-medium">By: {author}</p>
-                <div className="flex gap-5">
-                    <div className="flex gap-4 items-center">
-                        <p className="font-bold">Tag</p>
+                <div className="flex flex-col md:flex-row gap-5">
+                    <div className="flex flex-wrap gap-4 items-center">
+                        <p className="font-bold underline md:no-underline">Tag</p>
                         <h2 className="flex gap-2">{tags.map((tag, index) => <span
-                            className="text-[#23BE0A] font-medium bg-[#23be0a0d] px-4 py-2 rounded-xl"
+                            className="text-[#23BE0A] font-medium bg-[#23be0a0d] md:px-4 p-1 md:py-2 rounded-xl"
                             key={index}
                         >#{tag}</span>)}</h2>
                     </div>
@@ -29,7 +29,7 @@ const Read = ({ read }) => {
                     </div>
                 </div>
 
-                <div className="flex  gap-5 text-[#13131399] font-medium">
+                <div className="flex flex-col md:flex-row  gap-5 text-[#13131399] font-medium">
                     <div className="flex items-center gap-2">
                         <IoPeopleOutline />
                         Publisher: {publisher}
@@ -40,11 +40,10 @@ const Read = ({ read }) => {
                     </div>
                 </div>
                 <hr />
-                <div className="flex font-medium items-center gap-10">
+                <div className="flex flex-wrap font-medium items-center gap-3 md:gap-10">
                     <h3 className="bg-[#328eff26] px-5 py-2 rounded-full text-[#328EFF]">Category: {category}</h3>
                     <h3 className="px-5 text-[#FFAC33] py-2 rounded-full bg-[#ffac3326]">Rating: {rating}</h3>
                     
-                    {/* to={`/bookdetails/${bookId}` */}
                     <Link to={`/bookdetails/${bookId}`}><button className="px-5 text-white py-2 rounded-full bg-[#23BE0A]">View Details</button></Link>
                 </div>
             </div>
