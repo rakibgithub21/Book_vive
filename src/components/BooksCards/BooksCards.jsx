26322a8bd91f0f5e13ee676d7d1ed409267c37e8
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-26 08:58:44 +0600

    load all books fake data

--- src/components/BooksCards/BooksCards.jsx
+++ src/components/BooksCards/BooksCards.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 
 
 const BooksCards = () => {
+    const [books, setBooks] = useState();
+    useEffect(() => {
+        fetch('allbooks.json')
+            .then(res => res.json())
+            .then(data => setBooks(data))
+    }, [])
+    console.log(books);
     return (
         <div className="mt-14">
             <h3 className="text-center font-bold text-4xl">Books</h3>
