b08a99bb7daf049713825abf5c9b33315037eb89
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-26 08:52:47 +0600

    in home section given margin top

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Banner from "../components/Banner/Banner";
 
 const Home = () => {
     return (
-        <div>
+        <div className="mt-10">
             {/* Banner */}
             <Banner></Banner>
         </div>
