69c70da345f3af3a8525a371b027607e3d164504
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-26 09:05:12 +0600

    install rect proptypes

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,11 +1,16 @@
+import PropTypes from 'prop-types';
 
-
-const Book = ({book}) => {
+const Book = ({ book }) => {
+    console.log(book);
     return (
         <div>
             
         </div>
     );
+};
+
+Book.propTypes = {
+    book: PropTypes.object
 };
 
 export default Book;
