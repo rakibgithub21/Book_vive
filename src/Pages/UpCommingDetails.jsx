73bb0efd7ebc2058fb58c7a12bafcff1b287cf27
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-27 09:58:42 +0600

    add a button

--- src/Pages/UpCommingDetails.jsx
+++ src/Pages/UpCommingDetails.jsx
@@ -36,6 +36,12 @@ const UpCommingDetails = () => {
                         className="block border border-lime-600 w-full px-4 py-3 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none"
                         rows="5"
                     ></textarea>
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                        Submit
+                    </button>
                 </div>
             </div>
         </div>
