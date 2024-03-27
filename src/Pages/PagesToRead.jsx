e934a380f808198d8e02bcf6b5c7c2fbd7a82e4a
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-27 08:28:48 +0600

    chart responsive

--- src/Pages/PagesToRead.jsx
+++ src/Pages/PagesToRead.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+
+import  { useEffect, useState } from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import EmptyBar from "./EmptyBar";
+
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
 const getPath = (x, y, width, height) => {
     return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
-  ${x + width / 2}, ${y}
-  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
-  Z`;
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
 };
 
 const TriangleBar = (props) => {
@@ -15,41 +18,43 @@ const TriangleBar = (props) => {
     return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
 };
 
-
 const PagesToRead = () => {
     const [readLs, setReadLs] = useState([]);
+    const [showXAxis, setShowXAxis] = useState(true);
 
     useEffect(() => {
         const saveData = JSON.parse(localStorage.getItem('readList')) || [];
-        // console.log(saveData);
-        setReadLs(saveData)
-    }, [])
-    console.log(readLs);
+        setReadLs(saveData);
+        const handleResize = () => {
+            setShowXAxis(window.innerWidth > 768); 
+        };
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
-        <div>
-            {
-                readLs.length === 0 ? <EmptyBar></EmptyBar> : <BarChart
-                    width={1000}
-                    height={700}
-                    data={readLs}
-                    margin={{
-                        top: 20,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="bookName" />
-                    <Tooltip></Tooltip>
-                    <YAxis />
-                    <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-                        {readLs.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                        ))}
-                    </Bar>
-                </BarChart>
-            }
+        <div style={{ width: '100%', height: '400px' }}>
+            {readLs.length === 0 ? (
+                <EmptyBar />
+            ) : (
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        data={readLs}
+                        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        {showXAxis && <XAxis dataKey="bookName" />}
+                        <YAxis />
+                        <Tooltip />
+                        <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {readLs.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </ResponsiveContainer>
+            )}
         </div>
     );
 };
